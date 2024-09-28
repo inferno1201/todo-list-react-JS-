8d2b2ce5f34598ace17188e8ac54d27707973146
inferno1201 <neerajbora1201@gmail.com>
2024-09-28 20:20:14 +0530

    added UI

---
+++ src/components/AddItem.jsx
@@ -0,0 +1,22 @@
+import "../App.css"
+const AddItem = () => {
+  return (
+    <>
+      <div className="row">
+        <div className="col-4">
+          <input type="text" placeholder="enter task here"></input>
+        </div>
+        <div className="col-3">
+          <input type="date"></input>
+        </div>
+        <div className="col-2">
+          <button type="button" className="btn btn-success">
+            Add Task
+          </button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default AddItem;
---
+++ src/components/Task2.jsx
@@ -0,0 +1,17 @@
+const Task2 = () => {
+    return (
+      <>
+        <div className="row">
+          <div className="col-4"> Complete task 2</div>
+          <div className="col-3">29-09-2024</div>
+          <div className="col-2">
+            <button type="button" className="btn btn-danger">
+              delete
+            </button>
+          </div>
+        </div>
+      </>
+    );
+  };
+  
+  export default Task2;
